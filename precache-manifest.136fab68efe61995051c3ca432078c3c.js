825aaf6b32639c0c3ccbdb37e8932cc1bcd00f74
Deyan Ananiev <deyan.ananiev@gmail.com>
2020-09-28 14:09:43 +0300

    Updates

---
+++ precache-manifest.136fab68efe61995051c3ca432078c3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0716486630b416a62e96e69f7eee4a",
+    "url": "/react-pokemon-app/index.html"
+  },
+  {
+    "revision": "658b99b72b62c2347d45",
+    "url": "/react-pokemon-app/static/css/main.dea1f394.chunk.css"
+  },
+  {
+    "revision": "3c7751a6c8346dc7974d",
+    "url": "/react-pokemon-app/static/js/2.354cbe84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pokemon-app/static/js/2.354cbe84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "658b99b72b62c2347d45",
+    "url": "/react-pokemon-app/static/js/main.52d7b421.chunk.js"
+  },
+  {
+    "revision": "d44f2446001b93edb714",
+    "url": "/react-pokemon-app/static/js/runtime-main.24cf928f.js"
+  }
+]);
